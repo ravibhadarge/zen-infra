870b8e535b85013564655c7cdf4c400b627203f1
RAVI BHADARGE <ravibhadarge15@gmail.com>
2026-08-09 09:54:40 +0530

    Change instance type from t3.small to c7i-flex.large

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -26,7 +26,7 @@ module "eks" {
   vpc_id             = module.vpc.vpc_id
   subnet_ids         = module.vpc.private_subnets
   kubernetes_version = "1.34"
-  instance_types     = ["t3.small"]
+  instance_types     = ["c7i-flex.large"]
   min_size           = 1
   max_size           = 4
   desired_size       = 1
